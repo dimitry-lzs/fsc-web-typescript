51d0120e595b7d6984f335553b9f08826a008e05
dimitry-lzs <lazanasdimitrios@gmail.com>
2023-02-20 11:41:39 +0200

    changes

--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -6,6 +6,7 @@ function SearchResult() {
             <img src='https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg' className='MoviePoster' />
             <div className='MovieDetails'>
                 <div className='MovieTitle'>Titanic</div>
+                <div className='MovieYear'>1997</div>
                 <div className='MovieDescription'>A Great Movie about boat that sunk in the middle of the Atlantic Ocean</div>
             </div>
         </div>
