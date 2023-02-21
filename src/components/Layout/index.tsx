1215dc128dcb45116d0d907e148d77f2f07241b6
dimitry-lzs <lazanasdimitrios@gmail.com>
2023-02-21 02:18:01 +0200

    add resize listener for adding class for mobile devices

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import Header from '../Header';
 import ScrollToTop from '../ScrollToTop';
 import './Layout.less';
@@ -7,11 +9,29 @@ type ContainerProps = {
 };
 
 function Layout(props: ContainerProps) {
+    const [mobile, setMobile] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (window.innerWidth < 970) setMobile(true);
+
+        function resizer() {
+            if (window.innerWidth < 970) setMobile(true);
+            if (window.innerWidth >= 970) setMobile(false);
+        }
+
+        window.addEventListener('resize', resizer);
+
+        return () => window.removeEventListener('resize', resizer);
+    }, [navigate]);
+
     return (
         <>
             <ScrollToTop />
             <Header />
-            <main id='App'>{props.children}</main>
+            <main id='App' className={mobile ? 'mobile' : ''}>
+                {props.children}
+            </main>
         </>
     );
 }
